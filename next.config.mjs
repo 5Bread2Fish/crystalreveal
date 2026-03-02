78f8a2fcd30c0b7c73a5eb3feb6640488d230545
Justin Hyeon <justinhyeon@Justins-Humanscape-Mac-Air-2.local>
2026-03-02 19:06:51 +0700

    fix: prisma external package size and force-dynamic

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+    },
     images: {
         remotePatterns: [
             {
